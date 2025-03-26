cc6aede4eb67ef2d8d5c57de6e056b6d04e6790e
alter-sunset <87358040+alter-sunset@users.noreply.github.com>
2025-03-26 11:04:42 +0300

    add semaphores

--- DriveFromOutsideServer/Revit/RevitInstance.cs
+++ DriveFromOutsideServer/Revit/RevitInstance.cs
@@ -6,8 +6,9 @@ namespace DriveFromOutsideServer.Revit
     {
         private const int _latestVersion = 2025;
         private readonly Process _revitProcess = new();
+        private readonly SemaphoreSlim _semaphore;
         public int Version { get; private set; }
-        public RevitInstance(int version)
+        public RevitInstance(int version, SemaphoreSlim semaphore)
         {
             if (version < 2019 || version > _latestVersion)
             {
@@ -17,6 +18,8 @@ namespace DriveFromOutsideServer.Revit
                 );
             }
 
+            _semaphore = semaphore;
+            _semaphore.Wait();
             Version = version;
             _revitProcess.StartInfo.FileName = $"C:\\Program Files\\Autodesk\\Revit {Version}\\Revit.exe";
             _revitProcess.StartInfo.Arguments = $"D:\\test {Version}\\journal.txt"; //replace as something like AppData
@@ -26,6 +29,7 @@ namespace DriveFromOutsideServer.Revit
 
         public void Dispose()
         {
+            _semaphore.Release();
             _revitProcess.Kill();
             _revitProcess.Dispose();
         }
--- DriveFromOutsideServer/Services/DistributionService.cs
+++ DriveFromOutsideServer/Services/DistributionService.cs
@@ -9,15 +9,34 @@ namespace DriveFromOutsideServer.Services
         private readonly ILogger<DistributionService> _logger = logger;
         private readonly AssignmentContext _db = db;
         private ConcurrentBag<RevitInstance> _revitInstances = [];
+        private const int MAX_INSTANCES = 5;
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                IQueryable<KingAssignment> assignments = _db.Kings.Where(e => e.Status == AssignmentStatus.New);
+                IOrderedQueryable<KingAssignment> assignments = _db.Kings
+                    .Where(e => e.Status == AssignmentStatus.New)
+                    .OrderByDescending(e => e.Version);
 
+                //should i move initialization to class?
+                SemaphoreSlim semaphoreAssignment = new(MAX_INSTANCES);
+                SemaphoreSlim semaphoreRevit = new(MAX_INSTANCES);
 
+                ConcurrentQueue<Task> tasks = new();
+                foreach (KingAssignment assignment in assignments)
+                {
+                    Task task = new(() =>
+                    {
+                        semaphoreAssignment.Wait();
+                        //check for correct version and available slot
+                        if (_revitInstances.Count < MAX_INSTANCES)
+                        {
+                            _revitInstances.Add(new RevitInstance(assignment.Version, semaphoreRevit));
+                        }
+                    });
+                }
 
-                //add couple semaphores 1st for assignmeents, 2nd for revits
+                //add couple semaphores: 1st for assignmeents, 2nd for revits
                 //use SemaphoreSlim
                 //get kings
                 //run revits
