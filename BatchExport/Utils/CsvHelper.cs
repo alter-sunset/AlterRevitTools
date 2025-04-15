2c79475fde804c6f379e89d0bef1c392f861625e
alter-sunset <87358040+alter-sunset@users.noreply.github.com>
2025-04-15 13:30:04 +0300

    Fix csv serialization (#23)
    * update delimeter in csv, update erase from in params VM
    
    * trim parameter value
    
    * fix param.Value trim

--- BatchExport/Utils/CsvHelper.cs
+++ BatchExport/Utils/CsvHelper.cs
@@ -13,12 +13,12 @@ namespace AlterTools.BatchExport.Utils
         public CsvHelper(string csvFilePath, string[] parametersNames)
         {
             _stream = new(csvFilePath);
-            _stream.WriteLine(string.Join(";", headerBase.Concat(parametersNames))); // Header
+            _stream.WriteLine(string.Join("|", headerBase.Concat(parametersNames))); // Header
         }
 
         public void WriteElement(ParametersTable paramsTable)
             => _stream.WriteLine(
-                    string.Join(";",
+                    string.Join("|",
                         new string[] { paramsTable.ModelName, paramsTable.ElementId.ToString() }.Concat(paramsTable.Parameters.Values)));
 
         public void Dispose() => _stream.Dispose();
