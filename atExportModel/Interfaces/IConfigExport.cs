43344df8bbda9c9cf6b00d4bad9d9aa008ddaa79
alter-sunset <87358040+alter-sunset@users.noreply.github.com>
2026-07-27 15:46:44 +0300

    describe first version of main window

--- atExportModel/Interfaces/IConfigExport.cs
+++ atExportModel/Interfaces/IConfigExport.cs
@@ -4,13 +4,13 @@ namespace AlterTools.atExportModel.Interfaces;
 
 public interface IConfigExport
 {
-    public bool ExportRvt { get; set; }
+    public bool ExportRVT { get; set; }
     public RvtExportMode RvtExportMode { get; set; }
 
-    public bool ExportNwc { get; set; }
+    public bool ExportNWC { get; set; }
     public IConfigNWC ConfigNWC { get; set; }
 
-    public bool ExportIfc { get; set; }
+    public bool ExportIFC { get; set; }
     public IConfigIFC ConfigIFC { get; set; }
 
     public bool CleanModel { get; set; }
