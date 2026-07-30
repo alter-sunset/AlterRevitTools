33665ad3420c4a0f4b61f7c8f378d028941f9619
alter-sunset <87358040+alter-sunset@users.noreply.github.com>
2026-07-30 11:29:08 +0300

    update MainWindow

--- atExportModel/MainViewModel.cs
+++ atExportModel/MainViewModel.cs
@@ -10,20 +10,25 @@ public class MainViewModel : NotifyPropertyChanged, IConfigExport
 {
     private readonly ExternalEventHandler _handler;
 
-    public bool ExportRVT { get; set; }
+    public bool ExportRVT { get; set; } = false;
     public RvtExportMode RvtExportMode { get; set; }
 
-    public bool ExportNWC { get; set; }
+    public bool ExportNWC { get; set; } = false;
     public IConfigNWC ConfigNWC { get; set; }
     public ViewModelNWC ViewModelNWC { get; }
 
-    public bool ExportIFC { get; set; }
+    public bool ExportIFC { get; set; } = false;
     public IConfigIFC ConfigIFC { get; set; }
     public IConfigIFCAdditionalFields ConfigIFCAdditionalFields { get; set; }
     public ViewModelIFC ViewModelIFC { get; }
 
-    public bool CleanModel { get; set; }
+    public bool CleanModel { get; set; } = false;
     public IConfigClean ConfigClean { get; set; }
+
+    public string ViewName { get; set; } = "Navisworks";
+    public string FolderPathRVT { get; set; }
+    public string FolderPathNWC { get; set; }
+    public string FolderPathIFC { get; set; }
 
     public MainViewModel(ExternalEventHandler handler)
     {
