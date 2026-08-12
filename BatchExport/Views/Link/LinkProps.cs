83df8b0b0587884593979aef800281cbe8e4039b
alter-sunset <87358040+alter-sunset@users.noreply.github.com>
2026-08-12 16:47:53 +0300

    remove old Link code

--- BatchExport/Views/Link/LinkProps.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AlterTools.BatchExport.Views.Link;
-
-public class LinkProps(WorksetTable table, bool setWorksetId, bool pinLinks, string[] worksetPrefixes)
-{
-    public WorksetTable WorksetTable { get; } = table;
-    public bool SetWorksetId { get; } = setWorksetId;
-    public bool PinLink { get; } = pinLinks;
-    public string[] WorksetPrefixes { get; } = worksetPrefixes;
-}
