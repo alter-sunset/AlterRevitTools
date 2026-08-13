338922203d3338803de2e640846aca10a05f0830
alter-sunset <87358040+alter-sunset@users.noreply.github.com>
2026-08-13 15:04:27 +0300

    Delete DriveFromOutside directory

--- DriveFromOutside/Events/IEventHolder.cs
+++
@@ -1,9 +0,0 @@
-﻿using Autodesk.Revit.UI;
-
-namespace AlterTools.DriveFromOutside.Events;
-
-public interface IEventHolder
-{
-    public ExternalEvents ExternalEvent { get; }
-    public IExternalEventHandler ExternalEventHandler { get; }
-}
--- DriveFromOutside/Events/Transmit/TransmitConfig.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AlterTools.DriveFromOutside.Events.Transmit;
-
-public abstract class TransmitConfig
-{
-    public string[] Files { get; set; } = [];
-    public string FolderPath { get; set; } = string.Empty;
-}
